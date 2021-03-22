b1ab3ee507e22e832c16a37e64b8d67cf47508e1
Ézhor Malkávar <ezhor@PENUMBRA-II>
2021-03-22 15:12:11 +0100

    Alternative java execution.

--- client-start.sh
+++ client-start.sh
@@ -1,3 +1,3 @@
 git pull
-java --module-path /usr/share/openjfx/lib --add-modules ALL-MODULE-PATH -jar ./client/out/artifacts/client_jar/client.jar
+java --module-path /usr/share/openjfx/lib --add-modules=javafx.controls,javafx.fxml,javafx.base,javafx.media,javafx.web,javafx.swing -jar ./client/out/artifacts/client_jar/client.jar
 nc -u -l 2222 | mplayer -fps 200 -demuxer h264es -
