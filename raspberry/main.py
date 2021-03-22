f1644ee19b1176fe1d6934ea1c31fe7d1155a50e
Ézhor Malkávar <ezhor@PENUMBRA-II>
2021-03-22 19:14:22 +0100

    Increased streaming resolution.

--- raspberry/main.py
+++ raspberry/main.py
@@ -1,7 +1,7 @@
 import subprocess
 import socket
 
-cameraCommand = "/opt/vc/bin/raspivid -t 0 -w 300 -h 300 -hf -fps 20 -o - | nc -u 192.168.0.15 2222"
+cameraCommand = "/opt/vc/bin/raspivid -t 0 -w 1280 -h 720 -hf -fps 20 -o - | nc -u 192.168.0.15 2222"
 subprocess.Popen(cameraCommand.split(), stdout=subprocess.PIPE, shell=True)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
