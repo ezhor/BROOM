739faf6ef7146efe10abfbfa44c81db7e6907327
Ézhor Malkávar <ezhor@PENUMBRA-II>
2021-03-22 18:43:13 +0100

    First attempt to read all data received from client to raspberry.

--- raspberry/main.py
+++ raspberry/main.py
@@ -1,5 +1,6 @@
 import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(("192.168.0.15", 2727))
-data = sock.recv(16)
-print(data)
+while True:
+    data = sock.recv(1)
+    print(data)
