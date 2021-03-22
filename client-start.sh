413b1c40f06469b1dcab17bc764f9e5ecfeda54c
Ézhor Malkávar <ezhor@PENUMBRA-II>
2021-03-22 14:42:09 +0100

    Updated shell script to use JavaFX modules.

--- client-start.sh
+++ client-start.sh
@@ -1,3 +1,3 @@
 git pull
-java -jar ./client/out/artifacts/client_jar/client.jar
+java --module-path /usr/share/openjfx/lib --add-modules ALL-MODULE-PATH -jar ./client/out/artifacts/client_jar/client.jar
 nc -u -l 2222 | mplayer -fps 200 -demuxer h264es -
