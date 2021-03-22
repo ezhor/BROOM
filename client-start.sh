64b2d55a1fd96d2268e0395b53a933b29d293e45
Ézhor Malkávar <ezhor@PENUMBRA-II>
2021-03-22 21:22:56 +0100

    Changed process execution order in client-start.sh

--- client-start.sh
+++ client-start.sh
@@ -1,6 +1,6 @@
 git pull
-nc -u -l 2222 | mplayer -noconsolecontrols -fps 200 -demuxer h264es - &
-P1=$!
 java --module-path /usr/share/openjfx/lib --add-modules=javafx.controls,javafx.fxml,javafx.base,javafx.media,javafx.web,javafx.swing -jar ./client/out/artifacts/client_jar/client.jar &
+P1=$!
+nc -u -l 2222 | mplayer -noconsolecontrols -fps 200 -demuxer h264es - &
 P2=$!
 wait $P1 $P2
