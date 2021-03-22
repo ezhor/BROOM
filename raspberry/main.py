d1dc5be4b2f4f0feb8bb3f8b1c7f8d4968d0b1c1
Ézhor Malkávar <ezhor@PENUMBRA-II>
2021-03-22 18:46:50 +0100

    Line break message separation in raspberry-side.

--- raspberry/main.py
+++ raspberry/main.py
@@ -3,4 +3,9 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(("192.168.0.15", 2727))
 while True:
     data = sock.recv(1)
+    if data != "\n":
+        message += data
+    else:
+        print(message)
+        message = ""
     print(data)
