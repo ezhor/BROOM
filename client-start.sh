13f6a3d1cae0c69d727578352d741a834fd8dab7
Ézhor Malkávar <ezhor@PENUMBRA-II>
2021-03-22 21:53:24 +0100

    Now client-start.sh frees control port after execution.

--- client-start.sh
+++ client-start.sh
@@ -4,3 +4,4 @@ P1=$!
 java --module-path /usr/share/openjfx/lib --add-modules=javafx.controls,javafx.fxml,javafx.base,javafx.media,javafx.web,javafx.swing -jar ./client/out/artifacts/client_jar/client.jar &
 P2=$!
 wait $P1 $P2
+fuser -k 2727/tcp
