f279bf9d17e1b3e0ed6220bb6e949cc666b1cf1b
Ézhor Malkávar <ezhor@PENUMBRA-II>
2021-03-22 21:27:23 +0100

    Added some verbosity to rasberry side.

--- raspberry/main.py
+++ raspberry/main.py
@@ -4,8 +4,12 @@ import socket
 ps = subprocess.Popen("/opt/vc/bin/raspivid -t 0 -w 1280 -h 720 -hf -fps 20 -o -".split(), stdout=subprocess.PIPE)
 output = subprocess.check_output("nc -u 192.168.0.15 2222".split(), stdin=ps.stdout)
 
+print("Video started successfully")
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(("192.168.0.15", 2727))
+
+print("Control socket started successfully")
 message = ""
 while True:
     data = sock.recv(1)
