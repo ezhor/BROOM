819ddac81aacdb3c5f77fad39bdb4b3e0e32f613
Ézhor Malkávar <ezhor@PENUMBRA-II>
2021-03-22 19:00:32 +0100

    Shell mode activated in subprocess module.

--- raspberry/main.py
+++ raspberry/main.py
@@ -2,7 +2,7 @@ import subprocess
 import socket
 
 cameraCommand = "/opt/vc/bin/raspivid -t 0 -w 300 -h 300 -hf -fps 20 -o - | nc -u 192.168.0.15 2222"
-subprocess.Popen(cameraCommand.split(), stdout=subprocess.PIPE)
+subprocess.Popen(cameraCommand.split(), stdout=subprocess.PIPE, shell=True)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(("192.168.0.15", 2727))
