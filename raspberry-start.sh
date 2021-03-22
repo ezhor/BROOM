f937e66d4d4c123623d7fdcb34acbf41f5124ffd
Ézhor Malkávar <ezhor@PENUMBRA-II>
2021-03-22 21:42:34 +0100

    Process start are now handled by bash calls instead of Python calls.

--- raspberry-start.sh
+++ raspberry-start.sh
@@ -1,2 +1,8 @@
 git pull
-python ./raspberry/main.py
+(/opt/vc/bin/raspivid -t 0 -w 1280 -h 720 -hf -fps 20 -o - | nc -u 192.168.0.15 2222) &
+nc -u -l 2222 | mplayer -noconsolecontrols -fps 200 -demuxer h264es - &
+P1=$!
+python ./raspberry/main.py &
+P2=$!
+wait $P1 $P2
+
