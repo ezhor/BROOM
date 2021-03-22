c2e258af284421e4d63946db8702458e4fc7aeab
Ézhor Malkávar <ezhor@PENUMBRA-II>
2021-03-22 18:50:08 +0100

    Fixed wrong initialization syntax.

--- raspberry/main.py
+++ raspberry/main.py
@@ -1,6 +1,7 @@
 import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(("192.168.0.15", 2727))
+message = ""
 while True:
     data = sock.recv(1)
     if data != "\n":
