587c86d5b1199e2d2ff75a9ed84295cb273a73b7
Ézhor Malkávar <ezhor@PENUMBRA-II>
2021-03-22 21:21:29 +0100

    Added "noconsolecontrols" to mplayer command.

--- client-start.sh
+++ client-start.sh
@@ -1,5 +1,5 @@
 git pull
-nc -u -l 2222 | mplayer -fps 200 -demuxer h264es - &
+nc -u -l 2222 | mplayer -noconsolecontrols -fps 200 -demuxer h264es - &
 P1=$!
 java --module-path /usr/share/openjfx/lib --add-modules=javafx.controls,javafx.fxml,javafx.base,javafx.media,javafx.web,javafx.swing -jar ./client/out/artifacts/client_jar/client.jar &
 P2=$!
