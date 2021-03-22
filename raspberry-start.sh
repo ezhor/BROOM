e0ca6b9bebf53978cfb888ef26d8c54fca75ae08
Ézhor Malkávar <ezhor@PENUMBRA-II>
2021-03-22 21:49:02 +0100

    Removed bash command added by mistake.

--- raspberry-start.sh
+++ raspberry-start.sh
@@ -1,6 +1,5 @@
 git pull
 (/opt/vc/bin/raspivid -t 0 -w 1280 -h 720 -hf -fps 20 -o - | nc -u 192.168.0.15 2222) &
-nc -u -l 2222 | mplayer -noconsolecontrols -fps 200 -demuxer h264es - &
 P1=$!
 python ./raspberry/main.py &
 P2=$!
