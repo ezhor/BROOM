044b47035f0987ded8b3656337d5cdb927be4a69
Ézhor Malkávar <ezhor@PENUMBRA-II>
2021-03-22 21:09:29 +0100

    Propably fixed client-start.sh

--- client-start.sh
+++ client-start.sh
@@ -1,2 +1,6 @@
 git pull
-java --module-path /usr/share/openjfx/lib --add-modules=javafx.controls,javafx.fxml,javafx.base,javafx.media,javafx.web,javafx.swing -jar ./client/out/artifacts/client_jar/client.jar & nc -u -l 2222 | mplayer -fps 200 -demuxer h264es - && fg
+java --module-path /usr/share/openjfx/lib --add-modules=javafx.controls,javafx.fxml,javafx.base,javafx.media,javafx.web,javafx.swing -jar ./client/out/artifacts/client_jar/client.jar &
+P1=$!
+nc -u -l 2222 | mplayer -fps 200 -demuxer h264es - &
+P2=$!
+wait $P1 $P2
