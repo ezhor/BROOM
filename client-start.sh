a1d60c59d8e4ba4e2c3dcc6c546960c5537d7f1e
Ézhor Malkávar <ezhor@PENUMBRA-II>
2021-03-24 13:54:28 +0100

    Ports 2222/udp and 2727/tcp are now freed at the beginning of client-start.sh

--- client-start.sh
+++ client-start.sh
@@ -1,7 +1,8 @@
+fuser -k 2222/udp
+fuser -k 2727/tcp
 git pull
 nc -u -l 2222 | mplayer -noconsolecontrols -fps 200 -demuxer h264es - &
 P1=$!
 java --module-path /usr/share/openjfx/lib --add-modules=javafx.controls,javafx.fxml,javafx.base,javafx.media,javafx.web,javafx.swing -jar ./client/out/artifacts/client_jar/client.jar &
 P2=$!
 wait $P1 $P2
-fuser -k 2727/tcp
