7638f752ba404aca1b1b1b27f811c1094ccabc21
Ézhor Malkávar <ezhor@PENUMBRA-II>
2021-03-25 17:35:35 +0100

    Fixer serial port once again.

--- raspberry/SerialManager.py
+++ raspberry/SerialManager.py
@@ -2,7 +2,7 @@ import serial, time
 
 class SerialManager():
     def __init__(self):        
-        self.arduino = serial.Serial("/dev/ttyUSB1", 9600)
+        self.arduino = serial.Serial("/dev/ttyUSB0", 9600)
         time.sleep(1)
 
     def sendLine(self, line):
