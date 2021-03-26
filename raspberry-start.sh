22253716f7046c08d6d1d08924d5b498ad5a36bc
Ézhor Malkávar <ezhor@PENUMBRA-II>
2021-03-26 18:29:17 +0100

    Changed target IP to use DDNS.

--- raspberry-start.sh
+++ raspberry-start.sh
@@ -9,7 +9,7 @@ echo "Pulling from git..." > raspberry.log
 git pull
 echo "git pull OK" >> raspberry.log
 echo "Opening netcat from camera" >> raspberry.log
-(/opt/vc/bin/raspivid -t 0 -w 640 -h 460 -hf -fps 20 -o - | nc -u 192.168.0.15 2222) &
+(/opt/vc/bin/raspivid -t 0 -w 640 -h 460 -hf -fps 20 -o - | nc -u miranda-server.ddns.net 2222) &
 echo "Net camera OK" >> raspberry.log
 P1=$!
 echo "Opening Python communication" >> raspberry.log
