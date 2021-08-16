20bf68c04ec45a718c6004a42a9a184f73da94f4
Ézhor Malkávar <ezhormalkavar@gmail.com>
2021-08-16 23:03:38 +0200

    Port 2727 is now freed at the start of the execution.

--- raspberry-start.sh
+++ raspberry-start.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 echo "Raspberry Start" > raspberry.log
-echo "Killing previous raspivid and nc executions..." > raspberry.log
+
+echo "Freeing ports..." > raspberry.log
 killall raspivid
 killall nc
+fuser -k 2727/tcp
 echo "Killed OK" > raspberry.log
+
 echo "Pulling from git..." > raspberry.log
 git pull
 echo "git pull OK" >> raspberry.log
-echo "Opening netcat from camera" >> raspberry.log
+
+echo "Starting video streaming..." >> raspberry.log
 (/opt/vc/bin/raspivid -n -ih -t 0 -rot 0 -w 512 -h 288 -b 1000000 -fps 60 -o - | nc -lk4 5001) &
-echo "Net camera OK" >> raspberry.log
+echo "Video streaming OK" >> raspberry.log
 P1=$!
+
 echo "Opening Python communication" >> raspberry.log
 python ./raspberry/main.py &
 echo "Python OK" >> raspberry.log
 P2=$!
+
 wait $P1 $P2
 echo "Raspberry Finished" >> raspberry.log
