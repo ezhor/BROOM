3d58fcc9599de2efe2191ecb7882a72a44420e76
Ézhor Malkávar <ezhormalkavar@gmail.com>
2021-08-16 21:32:39 +0200

    Raspberry side in server mode.

--- raspberry/main.py
+++ raspberry/main.py
@@ -2,20 +2,26 @@ import subprocess
 import socket
 from SerialManager import SerialManager
 
+HOST = ""
+PORT = 2727
+
 print("Starting serial communication...")
 serialManager = SerialManager()
 print("Serial communication started successfully")
 
 print("Starting control socket...")
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(("192.168.0.30", 2727))
-print("Control socket started successfully")
-
 message = ""
-while True:
-    data = sock.recv(1)    
-    if data != "\n":
-        message += data
-    else:
-        serialManager.sendLine(message)
-        message = ""
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    while True:    
+        conn, addr = s.accept()
+        with conn:
+            print("Connected by ", addr)
+            while True:
+                data = conn.recv(1)
+                if data != "\n":
+                    message += data
+                else:
+                    serialManager.sendLine(message)
+                    message = ""
