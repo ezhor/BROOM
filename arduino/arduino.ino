e5c384940a8ba945c922d6b79ef314c7367bb620
Ézhor Malkávar <ezhormalkavar@gmail.com>
2021-08-21 12:24:57 +0200

    Updated Arduino firmware to use low-traffic control data.

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -5,11 +5,12 @@ Servo steeringServo;
 Servo cameraRotationXServo;
 Servo cameraRotationYServo;
 
-int motorPower;
-int steering;
-int cameraRotationX;
-int cameraRotationY;
+byte motorPower;
+byte steering;
+byte cameraRotationX;
+byte cameraRotationY;
 bool led;
+byte data[5];
 
 void setup() {
   motorPWM.attach(3);
@@ -31,23 +32,24 @@ void setup() {
 
 void loop() {
   if (Serial.available()) {
-    parseData(Serial.readStringUntil('\n'));
+    Serial.readBytesUntil('\n', data, 5);
+    parseData();
     sendPWM();
   }
 }
 
-void parseData(String data) {
-  motorPower = data.substring(0, 4).toInt();
-  steering = data.substring(4, 8).toInt();
-  cameraRotationX = data.substring(8, 12).toInt();
-  cameraRotationY = data.substring(12, 16).toInt();  
-  led = data.substring(16, 17) == "1";
+void parseData() {
+  motorPower = data[0];
+  steering = data[0];
+  cameraRotationX = data[0];
+  cameraRotationY = data[0];  
+  led = data[0] == 1;
 }
 
 void sendPWM() {
   motorPWM.writeMicroseconds(map(motorPower, -100, 100, 1000, 2000));
-  steeringServo.write(map(steering, 0, 180, 135, 45));
-  cameraRotationXServo.write(map(cameraRotationX, 0, 180, 180, 0));
-  cameraRotationYServo.write(map(cameraRotationY, 0, 180, 180, 0));
+  steeringServo.write(map(steering, -100, 100, 180, 0));
+  cameraRotationXServo.write(map(cameraRotationX, -100, 100, 180, 0));
+  cameraRotationYServo.write(map(cameraRotationY, -100, 100, 180, 0));
   digitalWrite(12, led ? HIGH : LOW);
 }
