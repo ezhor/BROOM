f0322bb82c8bbce9e5e6b5b0b6d08382f4507054
Ézhor Malkávar <ezhormalkavar@gmail.com>
2021-08-28 17:08:25 +0200

    No need to log the message anymore.

--- raspberry/SerialManager.py
+++ raspberry/SerialManager.py
@@ -6,5 +6,4 @@ class SerialManager():
         time.sleep(1)
 
     def sendLine(self, line):
-        print("Line send: " + line[0:4] + "|" + line[4:8] + "|"+ line[8:12] + "|"+ line[12:16] + "|"+ line[16:17])
         self.arduino.write((line + "\n").encode())
