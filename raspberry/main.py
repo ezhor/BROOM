0388a7ab073f63e8dbc52abb2a129343bba74aa2
Ézhor Malkávar <ezhormalkavar@gmail.com>
2022-04-02 21:34:53 +0200

    Removed unused variable

--- raspberry/main.py
+++ raspberry/main.py
@@ -11,7 +11,6 @@ serialManager = SerialManager()
 print("Serial communication started successfully")
 
 print("Starting control socket...")
-message = ""
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind((HOST, PORT))
 print("Control socket started")
@@ -20,7 +19,7 @@ while True:
         data, address = s.recvfrom(1024)
         print("Length: " + str(len(data)))
         if(len(data) == PACKET_LENGTH ):
-            serialManager.sendData(message)
+            serialManager.sendData(data)
     except Exception as e:
         print(e)
 if(s != null):
